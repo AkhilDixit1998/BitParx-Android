1037b6fee9ccdb123f783f405e9d946926ef7ab7
akhildixit1998 <worldincontrol@gmail.com>
2018-04-19 22:54:43 +0530

    added data in buy sell

--- app/src/main/java/com/akhil/akhildixit/bits/fragments/Home_Fragment.java
+++ app/src/main/java/com/akhil/akhildixit/bits/fragments/Home_Fragment.java
@@ -1,66 +1,132 @@
 package com.akhil.akhildixit.bits.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.akhil.akhildixit.bits.R;
+import com.akhil.akhildixit.bits.SettersGetters.BuySell;
+import com.akhil.akhildixit.bits.adapters.CustomBuySell;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.widget.GridLayout.HORIZONTAL;
 
 
 public class Home_Fragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public Home_Fragment() {
-    }
-
-
-
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<BuySell> arrayList=new ArrayList<>(10);
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        fetchInitialData();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView=view.findViewById(R.id.buySellList);
+
+        mLayoutManager=new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setVisibility(View.GONE);
+
+        return view;
     }
 
+    @SuppressLint("StaticFieldLeak")
+    public void fetchInitialData()
+    {
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+       new AsyncTask<Object,Void,String>()
+       {
+
+           @Override
+           protected void onPostExecute(String s) {
+               super.onPostExecute(s);
+
+               Log.e("Data","D "+s);
+               try {
+                   JSONArray jsonArray=new JSONArray(s);
+                   for (int i=0;i<jsonArray.length();i++)
+                   {
+                       BuySell buySell=new BuySell();
+                       JSONObject jsonObject= jsonArray.getJSONObject(i);
+                       String id=jsonObject.getString("id");
+                       String price=jsonObject.getString("price_usd");
+                       buySell.id=id;
+                       buySell.price=price;
+                       arrayList.add(buySell);
+                       Log.e("The names are as ","data "+id+" "+price+" ");
+                   }
+               } catch (JSONException e) {
+                   e.printStackTrace();
+               }
+               Log.e("AR","size "+arrayList.size());
+               adapter=new CustomBuySell(arrayList);
+                   recyclerView.setAdapter(adapter);
+                   adapter.notifyDataSetChanged();
+               recyclerView.setVisibility(View.VISIBLE);
+
+
+
+
+           }
+
+           @Override
+           protected String doInBackground(Object... voids) {
+
+               try {
+                   String json;
+
+                   URL url=new URL("https://api.coinmarketcap.com/v1/ticker/?limit=5");
+                   HttpsURLConnection urlConnection=(HttpsURLConnection)url.openConnection();
+                   BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                   StringBuilder stringBuilder=new StringBuilder();
+                   String line;
+                   while ((line=bufferedReader.readLine())!=null)
+                   {
+                       stringBuilder.append(line).append("\n");
+                   }
+                   bufferedReader.close();
+                   json=stringBuilder.toString();
+                   return stringBuilder.toString();
+               }  catch (Exception e) {
+                   e.printStackTrace();
+               }
+               return null;
+           }
+
+
+       }.execute();
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
